f250f8249882cf7ce31070552cce0cda910576c3
Jesse Martinez <jessemartinezdesign@gmail.com>
2024-06-29 19:25:47 +0900

    Add birthday as default date

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,7 +10,7 @@ export default function Nav() {
       </div>
       <div className="relative flex items-center justify-self-center right-10">
         <button className="hover:bg-white/10 transition-all p-1 mr-3 aspect-square h-6 rounded-full"><ChevronLeftIcon/></button>
-        <h2 className="whitespace-nowrap">27 June 2024</h2>
+        <h2 className="whitespace-nowrap">10 July 2024</h2>
         <button className="hover:bg-white/10 transition-all p-1 ml-3 aspect-square h-6 rounded-full"><ChevronRightIcon/></button>
       </div>
     </nav>
