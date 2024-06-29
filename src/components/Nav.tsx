998af325e848cc9afdcc3dcb5c86b9e02259a2d3
Jesse Martinez <jessemartinezdesign@gmail.com>
2024-06-29 15:50:11 +0900

    Add logo, change product name

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ export default function Nav() {
     <nav className="grid grid-cols-[auto_1fr_auto] items-center w-full py-2 px-3">
       <div className="flex items-center">
         <img src="/logo.png" className="max-h-4 mr-2 mt-1"></img>
-        <h1 className="text-lg justify-self-start">Wingbeat</h1>
+        <h1 className="text-lg justify-self-start">Aero</h1>
       </div>
       <div className="relative flex items-center justify-self-center right-10">
         <button className="hover:bg-white/10 transition-all p-1 mr-3 aspect-square h-6 rounded-full"><ChevronLeftIcon/></button>
