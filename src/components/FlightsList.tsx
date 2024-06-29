ac213aed130fbb7a3cbac5b9906cddb6692a6308
Jesse Martinez <jessemartinezdesign@gmail.com>
2024-06-29 18:07:21 +0900

    Add conditional rendering in flight sidebar

--- src/components/FlightsList.tsx
+++ src/components/FlightsList.tsx
@@ -9,20 +9,31 @@ type FlightsProps = {
 }
 
 export default function Flights({nextFlights, aircraftSelected, rotation, setRotation}:FlightsProps) {
+  const hasRotation = () => {
+    return rotation.length;
+  }
 
   return(
     <div className="flex flex-col w-3/12 pl-1">
-      <h2 className="text-center mb-4 text-lg">All Flights</h2>
+      <h2 className="text-center mb-4 text-lg">
+        {hasRotation() ? "Choose Next Flight" : "All Flights"}
+      </h2>
       <div className="border border-white/20 rounded grow p-3 overflow-auto">
-        {nextFlights.map((flight, i) => (
-          <SmallFlightCard 
-            key={i}
-            flight={flight}
-            aircraftSelected={aircraftSelected}
-            rotation={rotation}
-            setRotation={setRotation}
-          />
-        ))}
+        {nextFlights.length > 0 ? (
+          nextFlights.map((flight, i) => (
+            <SmallFlightCard
+              key={i}
+              flight={flight}
+              aircraftSelected={aircraftSelected}
+              rotation={rotation}
+              setRotation={setRotation}
+            />
+          ))
+        ) : (
+          <div className="flex justify-center items-center h-full w-full">
+            <h2 className="text-center text-gray-500">No possible flights.</h2>
+          </div>
+        )}
       </div>
     </div>
   )
