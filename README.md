4cd10c48473c31daa6e67eb686d8ca3858675c0d
Jesse Martinez <jessemartinezdesign@gmail.com>
2024-06-30 19:57:08 +0900

    Add bars to utilization bar

--- README.md
+++ README.md
@@ -9,6 +9,8 @@
 
 # Things to add
 - More instructions
+- Drag and drop functionality
+- Removing intermediate flights
 - More complex error handling during fetch
 - Date picker to jump to a specific date to schedule flights
 - React Suspense to show skeletons of aircrafts and flights while fetching
--- src/components/UtilizationBar.tsx
+++ src/components/UtilizationBar.tsx
@@ -1,22 +1,69 @@
-import { useEffect } from "react"
-import { Flight } from "../types"
+import { useState, useEffect } from "react";
+import { Flight } from "../types";
 
 type UtilizationBarProps = {
-  rotation: Flight[]
-}
+  rotation: Flight[];
+};
 
-export default function UtilizationBar({rotation}:UtilizationBarProps) {
+type Bar = {
+  left: number;
+  width: number;
+  color: string;
+};
 
-  return(
-    <div className="px-3 py-2 bg-white/5 border-white/20 border rounded rounded-t-none left-0 bottom-0 w-full z-10 ">
+export default function UtilizationBar({ rotation }: UtilizationBarProps) {
+  const [bars, setBars] = useState<Bar[]>([]);
+
+  useEffect(() => {
+
+    const calculateBars = () => {
+      const bars: Bar[] = [];
+      const dayInSeconds = 24 * 60 * 60;
+
+      const createBar = (start: number, end: number, color: string): Bar => {
+        const left = (start / dayInSeconds) * 100;
+        const width = ((end - start) / dayInSeconds) * 100;
+        return { left, width, color };
+      };
+
+      rotation.forEach((flight, index) => {
+        const { departuretime, arrivaltime } = flight;
+
+        bars.push(createBar(departuretime, arrivaltime, "rgba(21, 128, 61, .8)"));
+
+        if (index < rotation.length - 1) {
+          const nextFlight = rotation[index + 1];
+          bars.push(createBar(arrivaltime, nextFlight.departuretime, "rgba(76, 29, 149, .6)"));
+        }
+      });
+
+      setBars(bars);
+    };
+
+    calculateBars();
+    
+  }, [rotation]);
+
+  return (
+    <div className="px-3 py-2 bg-white/5 border-white/20 border rounded rounded-t-none left-0 bottom-0 w-full z-10">
       <div className="timeline flex justify-between items-center mb-1">
         <p className="text-micro">00:00</p>
         <p className="text-micro">12:00</p>
         <p className="text-micro">23:59</p>
       </div>
-      <div className="colored-timeline h-2 bg-white/20 rounded relative overflow-hidden">
-        
+      <div className="colored-timeline h-2 bg-white/20 rounded relative overflow-hidden flex">
+        {bars.map((bar, index) => (
+          <div
+            key={index}
+            className="absolute h-full"
+            style={{
+              left: `${bar.left}%`,
+              width: `${bar.width}%`,
+              backgroundColor: bar.color,
+            }}
+          ></div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
